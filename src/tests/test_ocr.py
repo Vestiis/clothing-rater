cbebfb083cfeba388e15d5a0d6add6a4abd3ea92
Guillaume LIGNER <guillaume.ligner@gmail.com>
2022-02-20 12:47:54 +0100

    (fix) Fix test ocr image format test

--- src/tests/test_ocr.py
+++ src/tests/test_ocr.py
@@ -4,13 +4,16 @@ import pytest
 import requests
 from PIL import Image
 
-from src.ocr import Ocr
+from src.config import Config
+from src.ocr import get_ocr
 
 
 @pytest.mark.parametrize(
     "image_url", ["https://storage.googleapis.com/public-labels/IMG_9193.HEIC"],
 )
 def test_ocr_set_image_format_for_google_ocr(image_url: str):
-    image_bytes = Ocr.set_image_format_for_google_ocr(requests.get(image_url).content)
+    image_bytes = get_ocr().set_image_format_for_google_ocr(
+        requests.get(image_url).content
+    )
     image = Image.open(io.BytesIO(image_bytes))
-    assert image.format == "JPEG"
+    assert image.format == Config.Ocr.google_image_format
